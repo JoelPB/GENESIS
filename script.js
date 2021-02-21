a2111ed79723f4b3de442506b4cf4c70c2294e16
JoelPB <coffee.program.joel@gmail.com>
2021-02-21 18:31:36 -0300

    up to part 4

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 
+// Cria ordem aleatória de cores
 let shuffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4);
     order[order.length] = colorOrder;
@@ -22,7 +23,7 @@ let shuffleOrder = () => {
         lightColor(elementColor, Number(i) + 1);
     }
 }
-
+// Acende a próxima cor
 let lightColor = (element, number) => {
     number = number * 500;
     setTimeout(() =>{
@@ -32,4 +33,30 @@ let lightColor = (element, number) => {
     setTimeout(() => {
         element.classList.remove('selected');
     })
+}
+
+// Checa se os botões clicados são os mesmos da ordem gerado no jogo
+let checkOrder = () => {
+    for(let i in clickedOrder){
+        if(clickedOrder[i] != order[i]){
+            lose();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length){
+        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
+        nextLevel();
+    }
+}
+
+// Função para o clique do usuário
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    elementColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        elementColorElement(color).classList.remove('selected');
+    })
+
+    checkOrder();
 }
